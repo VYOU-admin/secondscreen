fac813b1bfaa38ef8e391f0f49da73276770c624
Tomor <tomor@local.dev>
2026-01-27 02:01:49 -0500

    Add extension sidebar route

---
+++ web/src/app/extension/sidebar/page.js
@@ -0,0 +1,8 @@
+export default function SidebarPage() {
+  return (
+    <div style={{ padding: 16, fontFamily: "system-ui" }}>
+      <h1>SecondScreen Sidebar</h1>
+      <p>/extension/sidebar is working.</p>
+    </div>
+  );
+}
